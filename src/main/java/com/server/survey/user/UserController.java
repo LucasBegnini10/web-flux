c82da6d3e2322ce6a9a5ac93f6478cc93d079141
LucasBegnini10 <begninilucas12@gmail.com>
2024-05-29 14:09:09 -0300

    feat: endpoint /me

--- src/main/java/com/server/survey/user/UserController.java
+++ src/main/java/com/server/survey/user/UserController.java
@@ -3,7 +3,10 @@ package com.server.survey.user;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -18,5 +21,10 @@ public class UserController {
     @GetMapping
     public Flux<User> getAll(){
         return this.userService.getAll();
+    }
+
+    @GetMapping("/me")
+    public Mono<User> getMe(ServerWebExchange serverWebExchange){
+        return Mono.just(Objects.requireNonNull(serverWebExchange.getAttribute("user")));
     }
 }
